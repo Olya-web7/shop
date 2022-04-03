d8e7df9bd1f8a690aa4fb51f4e14a55dc6c6462c
Olya <ola.shy@yahoo.com>
2022-04-03 22:01:51 +0300

    fix

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CartItemComponent } from './components/cart-item/cart-item.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CartItemComponent
+  ],
   imports: [
     CommonModule
   ]
