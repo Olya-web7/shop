6f55fcea47b2ed4209051ba6269e220660bb825e
Olya <ola.shy@yahoo.com>
2022-04-03 21:57:37 +0300

    fix

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -13,22 +13,18 @@ export class ProductsService {
       {
         id: 1,
         name: 'Milk',
-        description: 'Prostokvashino milk 2,5% 1.5L',
         price: 4,
         isAvailable: true,
-        // imageUrl: 'https://img.fozzyshop.com.ua/204151-large_default/moloko-prostokvashino-pasterizovannoe-25.jpg'
       },
       {
         id: 2,
-        name: 'Snickers',
-        description: 'Chocolate bar 112.5g',
+        name: 'Apple',
         price: 17,
         isAvailable: true,
       },      
       {
         id: 3,
-        name: 'Cowboy Hat',
-        description: 'American style cowboy hat',
+        name: 'Tea',
         price: 29,
         isAvailable: true,
       },
