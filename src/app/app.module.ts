3be8da8267d655e9e17b9ae60b871cc62617bef7
Olya <ola.shy@yahoo.com>
2022-04-03 21:19:39 +0300

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstComponent } from './first/first.component';
 import { ProductComponent } from './products/components/product/product.component';
 import { ProductListComponent } from './products/components/product-list/product-list.component';
 import { CartListComponent } from './cart/components/cart-list/cart-list.component';
@@ -18,7 +17,6 @@ import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent,
     ProductComponent,
     ProductListComponent,
     CartListComponent,    
