b15527500540b99bc7b407fe277bcbe26d569d55
Olya <ola.shy@yahoo.com>
2022-04-03 21:56:08 +0300

    fix

--- src/app/cart/components/cart-list/cart-list.component.ts
+++ src/app/cart/components/cart-list/cart-list.component.ts
@@ -12,4 +12,5 @@ export class CartListComponent implements OnInit {
   constructor() { }
   
   ngOnInit(): void { }  
+
 }
--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -16,7 +16,7 @@ export class ProductsService {
         description: 'Prostokvashino milk 2,5% 1.5L',
         price: 4,
         isAvailable: true,
-        imageUrl: 'https://img.fozzyshop.com.ua/204151-large_default/moloko-prostokvashino-pasterizovannoe-25.jpg'
+        // imageUrl: 'https://img.fozzyshop.com.ua/204151-large_default/moloko-prostokvashino-pasterizovannoe-25.jpg'
       },
       {
         id: 2,
@@ -24,7 +24,6 @@ export class ProductsService {
         description: 'Chocolate bar 112.5g',
         price: 17,
         isAvailable: true,
-        imageUrl: 'https://ua.all.biz/img/ua/catalog/22573963.jpg'
       },      
       {
         id: 3,
@@ -32,7 +31,6 @@ export class ProductsService {
         description: 'American style cowboy hat',
         price: 29,
         isAvailable: true,
-        imageUrl: 'https://www.u-buy.com.ua/productimg/?image=aHR0cHM6Ly9tLm1lZGlhLWFtYXpvbi5jb20vaW1hZ2VzL0kvNzE5RUhYcmNUWUwuX0FDX1VMMTUwMF8uanBn.jpg'
       },
     ];
   }
